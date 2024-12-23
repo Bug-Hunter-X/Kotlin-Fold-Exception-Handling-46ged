1aafd46a915f5aaf8d2389298562e220156e97a8
BugHunter-X <askzenai@gmail.com>
2024-12-23 04:48:05 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val sum = list.fold(0) { acc, i ->
+        if (i == 3) {
+            throw Exception("Three is not allowed")
+        } else {
+            acc + i
+        }
+    }
+    println(sum)
+}
